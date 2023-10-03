b1f580f7c40b9412b13f825cfc4d922b46e35443
Viii <vihirmujahid8@gmail.com>
2023-10-03 12:29:56 +0700

    first commit

---
+++ test/dart_application_pab_i3fb_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_application_pab_i3fb/dart_application_pab_i3fb.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
