432c57c075b630cae4a94ba21d8c3eb75afafd4a
Viii <vihirmujahid8@gmail.com>
2023-10-03 12:45:10 +0700

    Mengganti Kata Ke Hello Everyone

--- bin/dart_application_pab_i3fb.dart
+++ bin/dart_application_pab_i3fb.dart
@@ -1,1 +1,6 @@
-void main() {}
+import 'package:dart_application_pab_i3fb/dart_application_pab_i3fb.dart'
+    as dart_application_pab_i3fb;
+
+void main(List<String> arguments) {
+  print('Hello Everyone');
+}
